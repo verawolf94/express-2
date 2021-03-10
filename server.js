c5d15f00134d66d81ff0aa80e01f3375d5b77dca
verawolf94 <72465439+verawolf94@users.noreply.github.com>
2021-03-10 14:23:02 -0800

    adds codes

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-ar express = require('express');
+var express = require('express');
 
 var app = express();
 const PORT = 3000;
 
-p.listen(PORT,()=>{
-  console.log(`App is running on port ${PORT}`);
-});app.set('view engine','ejs');//sent views to EJS
+app.set('view engine','ejs');//sent views to EJS
 
 //this is how a route is built in Express
 app.get('/', (req,res)=>{
   var title = "Our Home Page";
   res.render('index',{title:title});
 });
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+});
+
+
